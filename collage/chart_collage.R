2bc3e366d829c1fd80abb818ac072c69a2ac522d
fgazzelloni <docksbox@pm.me>
2025-11-29 23:00:06 +0100

    collage

--- collage/chart_collage.R
+++ collage/chart_collage.R
@@ -1,20 +1,26 @@
 # Building a collage of all images
 library(tidyverse)
 
-dirs <- list.dirs("~/Documents/R/R_general_resources/EDA_and_maps/30DayMapChallenge/2024")
+dirs <- list.dirs("~/Documents/R/R_general_resources/EDA_and_maps/30DayMapChallenge/2025")
 files <- list.files(dirs)
 
 #Get the list of all images: (".jpg", ".png", etc) 
-allFiles <- list.files(dirs,
-                      pattern = "png|gif|jpg", 
-                      full.names = T)
+allFiles <- list.files(
+  dirs,
+  pattern = "^day[0-9]+_[A-Za-z0-9_\\-]+\\.(png|gif|jpg)$",
+  full.names = TRUE,
+  ignore.case = TRUE
+)
 
 library(fs)
 all_images<- allFiles %>% 
   path() %>%
   as_tibble() %>%
   mutate(fullpath = value,
-         filename = basename(value))
+         filename = basename(value),
+         filename = readr::parse_number(filename),
+         filename = sprintf("%02d", filename)) %>%
+  arrange(filename) 
 
 all_images1<-all_images %>%
   mutate(id=parse_number(filename),
@@ -30,7 +36,7 @@ tb_images <- matrix(all_images1$fullpath,
   as.data.frame() %>% 
   gt::gt()%>%
   gt_theme_538() %>%
-  tab_header(title = html("<strong>#30DayMapChallenge 2024</strong>")) %>%
+  tab_header(title = html("<strong>#30DayMapChallenge 2025</strong>")) %>%
   tab_source_note(source_note = md("by Federica Gazzelloni"))%>%
   tab_options(column_labels.hidden = TRUE) %>%
   gtExtras::gt_img_rows(columns=V1, 
@@ -52,6 +58,6 @@ tb_images <- matrix(all_images1$fullpath,
 
 library(webshot2)
 tb_images %>%
-  gtsave_extra("~/Documents/R/R_general_resources/EDA_and_maps/30DayMapChallenge/2024/collage2024.png",
+  gtsave_extra("~/Documents/R/R_general_resources/EDA_and_maps/30DayMapChallenge/2025/collage2025.png",
                vwidth = 1400)
 
